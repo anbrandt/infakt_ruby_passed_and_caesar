c167e2342a16f6ac410f726a54b77c0216e540f2
Andrzej Brandt <andrzej.brandt@gmail.com>
2018-05-22 23:11:12 +0200

    initial commit - both tasks are passing tests

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,22 @@
-class CeasarCipher
+class CaesarCipher
+  attr_reader :text_input, :factor
 
+  def initialize(text_input, factor = 13)
+    @text_input = text_input
+    @factor = factor
+  end
+
+  def perform
+    alphabet_downcase = ('a'..'z').to_a
+    alphabet_upcase = ('A'..'Z').to_a
+
+    downcase_hash_rotated = Hash[alphabet_downcase.zip(alphabet_downcase.rotate(factor))]
+    upcase_hash_rotated = Hash[alphabet_upcase.zip(alphabet_upcase.rotate(factor))]
+
+    merged_hash_encrypted = downcase_hash_rotated.merge(upcase_hash_rotated)
+    encrypted_arr = text_input.chars.map do |char|
+      merged_hash_encrypted.fetch(char, char)
+    end
+    encrypted_arr.join
+  end
 end
--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,29 @@
 class PassedOrFailed
+  attr_reader :results, :threshold
 
+  def initialize(results, threshold)
+    @results = results
+    @threshold = if threshold.class == Integer
+                   threshold
+                 else
+                   threshold.to_i
+                 end
+  end
+
+  def perform
+    passed = Hash.new
+    failed = Hash.new
+    @results.each do |key, value|
+      value_int = value.to_i
+      (value_int >= threshold) ? (passed[key] = value_int) : failed[key] = value_int
+      end
+    expected_hash = Hash.new
+    expected_hash[:passed] = passed
+    expected_hash[:failed] = failed
+    expected_hash
+  end
 end
+
+hash = { 'Mark' => '10', 'Ellen' => 65, 'Roger' => '20', 'Mike' => 70, 'Dan' => 99, 'Witt' => 10 }
+
+puts PassedOrFailed.new(hash, '55').perform
